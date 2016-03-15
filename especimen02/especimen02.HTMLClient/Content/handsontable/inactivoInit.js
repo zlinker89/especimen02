81259f4fa5132da839a2d6cf64b68bc389447620
zlinker89 <ospi89@hotmail.com>
2016-03-15 17:37:09 -0500

    add js

---
+++ especimen02/especimen02.HTMLClient/Content/handsontable/inactivoInit.js
@@ -0,0 +1,308 @@
+﻿var hot;
+// Obtengo los id de la evaluacion y competencia
+var vars = getVarsUrl();
+console.log(vars);
+// verifico si existe una sesion
+var sesion = JSON.parse(localStorage.getItem("usuario")) || null;
+console.log(sesion.tipo_usuario[0].tipo);
+if (sesion === null) {
+    location.href = "../login.html";
+} else if (sesion.tipo_usuario.length < 2) {
+    if (sesion.tipo_usuario[0].tipo !== 'Administrador') {
+        cerrarSession();
+    }
+} else {
+    var cont = 0;
+    for (var i in sesion.tipo_usuario) {
+        if (sesion.tipo_usuario[i] === 'Administrador') {
+            cont++;
+        }
+    }
+    if (cont > 0) {
+        cerrarSession();
+        location.href = "../login.html";
+    }
+}
+var url = '/api/logout/' + sesion.id;
+var empleado_seleccionado;
+var row;
+angular.module("miApp", []).controller("tabla", function ($scope, $http) {
+    $scope.tipos = "Administrador";
+    $scope.usuario = sesion;
+
+    $scope.LogOut = function () {
+        // cerramos session
+        cerrarSession();
+    };
+    $scope.CambiarPagina = function () {
+        // session
+        if ($scope.tipos == "Administrador") {
+            location.href = "/public_html/Empleados/indexempleado.html";
+        } else if ($scope.tipos == "Seguridad") {
+            location.href = "/public_html/seguridad/index.html";
+        } else {
+            location.href = "/public_html/login-empleados/evaluacion-empleados.html";
+        }
+    };
+    // fin session
+    $scope.enviado = false;
+
+    $scope.iniciar = function () {
+        /*SIMULACION LOCAL*/
+        $http.get('/EmpleadosInactivos/' + vars.periodo).then(function (d) {
+            var empleados = d.data;
+            var hotElement = document.getElementById('example');
+            hot = new Handsontable(hotElement, {
+                data: empleados,
+                columns: [
+                        {
+                            data: "id",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "cedula",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "Nombre",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "email",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "tipo",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "Departament",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "Area",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "SubArea",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "CrewCd",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "RosterPosition",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "Unit",
+                            type: "text",
+                            readOnly: true,
+                        }, {
+                            data: "liderado1",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "liderado2",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "liderado3",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "liderado4",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "liderado5",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "liderado6",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "liderado7",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "liderado8",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "liderado9",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "liderado10",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "par1",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "par2",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "par3",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "par4",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "par5",
+                            type: "text",
+                            readOnly: true,
+                        },
+                        {
+                            data: "Jefe",
+                            type: "text",
+                            readOnly: true,
+                        }
+                ],
+                stretchH: "all",
+                search:true,
+                rowHeaders: true,
+                columnSorting: true,
+                afterSelectionByProp: function (r1, c1, rf, ct) {
+                    row = r1;
+                    $("#nombre").val(empleados[r1].Nombre);
+                    $("#documento").val(empleados[r1].cedula);
+                    empleado_seleccionado = empleados[r1];
+                    $("#ventana2").modal("show");
+                },
+                colHeaders: [
+                    "ID",
+                    "Cedula",
+                    "Nombre",
+                    "Email",
+                    "Type",
+                    "Department",
+                    "Area",
+                    "SubArea",
+                    "Crew Cd",
+                    "Roster position",
+                    "Unit",
+                    "Liderado 1",
+                    "Liderado 2",
+                    "Liderado 3",
+                    "Liderado 4",
+                    "Liderado 5",
+                    "Liderado 6",
+                    "Liderado 7",
+                    "Liderado 8",
+                    "Liderado 9",
+                    "Liderado 10",
+                    "Par 1",
+                    "Par 2",
+                    "Par 3",
+                    "Par 4",
+                    "Par 5",
+                    "Jefe",
+                ],
+            });
+            // funciones
+            /*----------------BUSCAR EN TABLA----------------------------------------------------------------*/
+            var buscarField = document.getElementById("buscar");
+            // funciones
+            Handsontable.Dom.addEvent(buscarField, 'keyup', function (event) {
+                //                    debugger
+                hot.loadData(empleados);
+
+                var queryResult = hot.search.query(this.value);
+                rows = getRowsFromObjects(queryResult);
+                //console.log('searchFiled', buscarField.value);
+                //console.log('rows', rows);
+
+                //console.log('tData', empleados);
+                var filtered = empleados.filter(function (_, index) {
+                    return !buscarField.value || rows.indexOf(index) >= 0;
+                });
+                //console.log('filtered', filtered);
+
+                hot.loadData(filtered);
+
+                //                                        hot.render();
+            });
+            function getRowsFromObjects(queryResult) {
+                rows = [];
+                for (var i = 0, l = queryResult.length; i < l; i++) {
+                    //                        debugger
+                    rows.push(queryResult[i].row);
+                }
+                console.log('rows', rows);
+                return rows;
+            }
+            /*-------fin---------BUSCAR EN TABLA----------------------------------------------------------------*/
+            //console.log(hot.getDataAtRow(1));
+            $scope.AddEmpleado = function () {
+                    // AQUI EL CODIGO PARA ENVIAR el empleado AL SERVIDOR a seleccionados
+                    // en este punto se manda el id al server para remover el empleado
+                    var seleccionado = {
+                        id: null,
+                        id_empleados: empleado_seleccionado.id,
+                        id_periodos: Number(vars.periodo),
+                        estado: 1
+                    };
+                    console.log(seleccionado);
+                    $http.post('/AddEmpleados/', seleccionado).then(function (d) {
+                        hot.destroy();
+                        $scope.iniciar();
+                    });
+                }
+        });
+    };
+
+    $scope.iniciar();
+    
+
+
+
+});
+function cerrarSession() {
+    localStorage.removeItem("usuario");
+    location.href = "../login.html";
+}
+/*
+    * Funcion que recoje los valores pasados por get de una url.
+*/
+function getVarsUrl() {
+    var url = location.search.replace("?", "");
+    var arrUrl = url.split("&");
+    var urlObj = {};
+    for (var i = 0; i < arrUrl.length; i++) {
+        var x = arrUrl[i].split("=");
+        urlObj[x[0]] = x[1]
+    }
+    return urlObj;
+}
---
+++ especimen02/especimen02.HTMLClient/Content/jsPDF/ToPDF.js
@@ -0,0 +1,75 @@
+﻿function ToPDF() {
+    var pdf = new jsPDF('p', 'pt', 'letter');
+    // source can be HTML-formatted string, or a reference
+    // to an actual DOM element from which the text will be scraped.
+    source = $('#tabla')[0];
+
+    // we support special element handlers. Register them with jQuery-style
+    // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+    // There is no support for any other type of selectors
+    // (class, of compound) at this time.
+    specialElementHandlers = {
+        // element with id of "bypass" - jQuery style selector
+        '#bypassme': function (element, renderer) {
+            // true = "handled elsewhere, bypass text extraction"
+            return true
+        }
+    };
+    margins = {
+        top: 80,
+        bottom: 60,
+        left: 40,
+        width: 522
+    };
+    // all coords and widths are in jsPDF instance's declared units
+    // 'inches' in this case
+    pdf.fromHTML(
+    source, // HTML string or DOM elem ref.
+    margins.left, // x coord
+    margins.top, { // y coord
+        'width': margins.width, // max width of content on PDF
+        'elementHandlers': specialElementHandlers
+    },
+
+    function (dispose) {
+        // dispose: object with X, Y of the last line add to the PDF
+        //          this allow the insertion of new lines after html
+        
+                var svg = document.querySelector("svg");
+                if (svg) {
+                    var xml = new XMLSerializer().serializeToString(svg);
+                    // var data = "data:image/svg+xml;base64," + btoa(xml);
+                    canvg('canvas', xml); // envio imagen al canvas
+                    // obtengo la imagen
+                    // the canvas calls to output a png
+                    var canvas = document.getElementById("canvas");
+                    var img = canvas.toDataURL("image/png");
+                    // tomo fecha
+                    var d = new Date();
+                    pdf.addImage(img, 'jpg', 100, 350, 400, 400);
+                    pdf.setFontSize(16);
+                    pdf.text("Liderados", 430, 330);
+                    pdf.text("Jefe", 430, 350);
+                    pdf.text("Pares", 430, 370);
+                    pdf.text("Autoevaluación", 430, 390);
+                    // liderados
+                    pdf.setFillColor(237, 201, 81);
+                    pdf.circle(410, 323, 8, 'FD');
+                    // Jefe
+                    pdf.setFillColor(204, 51, 63);
+                    pdf.circle(410, 343, 8, 'FD');
+                    // Pares
+                    pdf.setFillColor(0, 160, 176);
+                    pdf.circle(410, 363, 8, 'FD');
+                    // autoevaluacion
+                    pdf.setFillColor(255, 170, 153);
+                    pdf.circle(410, 383, 8, 'FD');
+
+                    // save
+                    pdf.save('resultado' + d.getDate() + '_' + (d.getMonth() + 1) + '_' + d.getFullYear() + '__' + d.getHours() + '_' + d.getMinutes() + '_' + d.getMilliseconds() + '.pdf');
+                }
+           
+        
+        
+    }, margins);
+}
