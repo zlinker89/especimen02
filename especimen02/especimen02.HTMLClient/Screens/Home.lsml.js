5690b94fc9db52684796531c44d35c8c5fed5387
elkinpertuz6 <elccoco1995@hotmail.com>
2016-03-15 17:36:17 -0500

    vistas de administrador

---
+++ especimen02/especimen02.HTMLClient/Screens/Home.lsml.js
@@ -0,0 +1,37 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+myapp.Home.ShowBrowseEmpleadosSet_postRender = function (elemet, contenItem) {
+    $(element).find('a').css({
+        'background-image': 'url(Content/Images/user-logo.png)',
+        'background-repeat': 'no-repeat',
+        'background-position': 'left',
+        'width': '250px',
+        'height': '250px'
+    });
+};
+myapp.Home.ShowBrowseevaluacion_postRender = function (elemet, contenItem) {
+    $(element).find('a').css({
+        'background-image': 'url(Content/Images/user-logo.png)',
+        'background-repeat': 'no-repeat',
+        'background-position': 'left',
+        'width': '250px',
+        'height': '250px'
+    });
+};
+myapp.Home.ShowBrowseperiodosSet_postRender = function (elemet, contenItem) {
+    $(element).find('a').css({
+        'background-image': 'url(Content/Images/user-logo.png)',
+        'background-repeat': 'no-repeat',
+        'background-position': 'left',
+        'width': '250px',
+        'height': '250px'
+    });
+};
+myapp.Home.ShowBrowseResultadosSet_postRender = function (elemet, contenItem) {
+    $(element).find('a').css({
+        'background-image': 'url(Content/Images/user-logo.png)',
+        'background-repeat': 'no-repeat',
+        'background-position': 'left',
+        'width': '250px',
+        'height': '250px'
+    });
+};
