1280de61703cc6d02c682a6a7f5db9b3069e2fa4
zlinker89 <ospi89@hotmail.com>
2016-03-16 08:42:27 -0500

    recuperado

--- especimen02/especimen02.HTMLClient/Screens/Login.lsml.js
+++ especimen02/especimen02.HTMLClient/Screens/Login.lsml.js
@@ -1,39 +1,30 @@
 ﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
-
-myapp.Home.nombreusuario_render = function (element, contentItem) {
-    /* Modificar atributos
-    $(element).parent().css({
-        "background-color": "green",
-        "background-image": "none",
-        color: "yellow"
-    });
-    */
-    $(element).parent().css({
-        "font-weight": "700",
-    });
-    $(element).parent().find("label")[0].innerHTML += " <input id='nombre_usuario' type='text'/>";
-};
-
-myapp.Home.contrasena_render = function (element, contentItem) {
-    $(element).parent().css({
-        "font-weight": "700",
-    });
-    $(element).parent().find("label")[0].innerHTML += " <input id='contrasena' type='password'/>";
-};
-
-myapp.Home.LoginForm_postRender = function (element, contentItem) {
+/*------------------------Render Login--------------------------------*/
+myapp.Login.LoginForm_postRender = function (element, contentItem) {
     $(element).parent().css({
         "width": "80%",
         "margin": "0 auto"
     });
 };
-
+myapp.Login.nombreusuario_render = function (element, contentItem) {
+    $(element).parent().css({
+        "font-weight": "700",
+    });
+    $(element).parent().find("label")[0].innerHTML += " <input id='nombre_usuario' type='text'/>";
+};
+myapp.Login.contrasena_render = function (element, contentItem) {
+    $(element).parent().css({
+        "font-weight": "700",
+    });
+    $(element).parent().find("label")[0].innerHTML += " <input id='contrasena' type='password'/>";
+};
+/*-------------------FIN-----Render Login--------------------------------*/
 /*
 *   FUNCION para autenticar el usuario
 */
-myapp.Home.IniciarSesion_execute = function (screen) {
+myapp.Login.IniciarSesion_execute = function (screen) {
     var usuario = {
-        NewId:null,
+        NewId: null,
         NewNombre: $("#nombre_usuario").val(),
         NewContrasena: $("#contrasena").val(),
     };
@@ -46,4 +37,7 @@ myapp.Home.IniciarSesion_execute = function (screen) {
             alert("Resultado es" + JSON.stringify(d));
         }
     });
+};
+myapp.Login.IniciarSesion_postRender = function (element, contentItem) {
+    // hacer css
 };
--- especimen02/especimen02.Server/DataSources/Drummond02Data/_Drummond02DataService.lsml.vb
+++
@@ -1,7 +0,0 @@
-﻿Namespace LightSwitchApplication
-    Public Class Drummond02DataService
-        Private Sub empleado_CanRead(ByRef result As Boolean)
-            result = Me.Application.User.HasPermission(Permissions.CanViewEmpleado)
-        End Sub
-    End Class
-End Namespace
